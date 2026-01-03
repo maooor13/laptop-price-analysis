2dbda233d02616f2bb6f43b17278e7b546a4a156
Maor <maooor13@gmail.com>
2026-01-03 17:41:50 +0200

    add plot_os_dist by price

--- main.py
+++ main.py
@@ -25,13 +25,18 @@ def plot_avg_laptop_prices_by_company(laptop_data: pd.DataFrame):
     return fig
 
 
-'''
-Plot for each of the operating system types the distribution of the prices, so that
-the number of plots equals to the number of unique operating systems.
-'''
-def plot_os_distribution():
-    fig, ax = plt.subplot()
-    ax.hist(laptop)
+def plot_os_distribution_price(laptop_data: pd.DataFrame, os_name: str):
+    '''
+    Plot for each of the operating system types the distribution of the prices, so that
+    the number of plots equals to the number of unique operating systems.
+    '''
+    fig, ax = plt.subplots(figsize=(10,5))
+    os_data = laptop_data[laptop_data["OpSys"] == os_name]
+    ax.hist(os_data["Price (Euro)"], edgecolor="black")
+    ax.set_title(f"Distribution of Price for Operation System {os_name}")
+    ax.set_xlabel("Average Price (Euro)")
+    ax.set_ylabel("Operating System")
+    return fig
 
 
 def list_all_unique_opsys(laptop_data: pd.DataFrame):
@@ -49,18 +54,18 @@ def generalize_opsys(specific_opsys: pd.Series):
 
 
 def simplify_os(name: str):
-    name = name.lower()
-    if "windows" in name:
+    lowered_name = name.lower()
+    if "windows" in lowered_name:
         return "Windows"
-    if "mac" in name:
+    if "mac" in lowered_name:
         return "macOS"
-    return name.capitalize()
+    return name
 
 
 def generalize_column_opsys(laptop_data: pd.DataFrame):
-    df = laptop_data.copy()
-    df["OpSys"] = df["OpSys"].apply(simplify_os)
-    return df
+    data_copy = laptop_data.copy()
+    data_copy["OpSys"] = data_copy["OpSys"].apply(simplify_os)
+    return data_copy
 
 
 def load_laptop_data(datafile="laptop_price - dataset.csv") -> pd.DataFrame:
@@ -77,15 +82,14 @@ def load_laptop_data(datafile="laptop_price - dataset.csv") -> pd.DataFrame:
 
 def main():
     laptop_data = load_laptop_data()
-
+    new_laptop_data = generalize_column_opsys(laptop_data)
     # print(laptop_data)
     # plot_laptop_prices(laptop_data)
     # plot_avg_laptop_prices_by_company(laptop_data)
-    # plt.show()
-    opsys = list_all_opsys(laptop_data)
-    print(opsys[0])
-    fig, ax = plt.subplot()
-    ax.hist(laptop_data["OpSys"])
+    # plt.show()  
+    for os_name in list_all_unique_opsys(new_laptop_data):
+        print(plot_os_distribution_price(new_laptop_data, os_name))
+    plt.show()
 
 
 if __name__ == "__main__":
